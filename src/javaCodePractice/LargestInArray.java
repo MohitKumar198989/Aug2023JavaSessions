15a4d1cd78cede0369ad34c28dcd6e2779e791d4
MohitKumar198989 <mohit.kumar198989@gmail.com>
2024-10-06 15:37:07 +0530

    Update LargestInArray

--- src/javaCodePractice/LargestInArray.java
+++ src/javaCodePractice/LargestInArray.java
@@ -4,19 +4,30 @@ public class LargestInArray {
 
 	public static void main(String[] args) {
 		
-		 //Initialize array  
-		// int [] arr = {25, 11, 7, 75, 56}; 
-        int [] arr = new int [] {25, 11, 7, 75, 56};  
-        //Initialize max with first element of array.  
-        int max = arr[0];  
-        //Loop through the array  
-        for (int i = 0; i < arr.length; i++) {  
-            //Compare elements of array with max  
-           if(arr[i] > max)  
-               max = arr[i];  
-        }  
-        System.out.println("Largest element present in given array: " + max);  
+//		 //Initialize array  
+//		// int [] arr = {25, 11, 7, 75, 56}; 
+//        int [] arr = new int [] {25, 11, 7, 75, 56};  
+//        //Initialize max with first element of array.  
+//        int max = arr[0];  
+//        //Loop through the array  
+//        for (int i = 0; i < arr.length; i++) {  
+//            //Compare elements of array with max  
+//           if(arr[i] > max)  
+//               max = arr[i];  
+//        }  
+//        System.out.println("Largest element present in given array: " + max);  
+//        
+        
 
+		int[] arr = {1, 3, 5, 7, 9};
+		int largest = arr[0];
+		for (int num : arr) {
+		if (num > largest) {
+		largest = num;
+		}
+		}
+		System.out.println("Largest element present in given array: "+largest);
+		
 	}
 
 }
